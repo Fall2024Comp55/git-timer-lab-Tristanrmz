f76251476a4a324fbc6615663410b963bc914ed7
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-09-30 22:04:30 -0700

    all balls move across the screen

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -5,10 +5,27 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
+//when pushing add "all balls move across the screen"
 public class BallLauncher extends GraphicsProgram{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int SIZE = 25;
+	
+	
+	private ArrayList<GOval> balls;
+	public static final int Ms = 50;
+	public static final int SPEED = 2;
+	private Timer balltimer;
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		for(GOval ball:balls) {
+			ball.move(SPEED, 0);
+			
+		}
+		
+	}
+	
 	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -16,10 +33,22 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void run() {
+		balls = new ArrayList<GOval>(); //<GOval>
 		addMouseListeners();
+		balltimer = new Timer(Ms, this);
+		balltimer.start();
+		
 	}
 	
 	public void mousePressed(MouseEvent e) {
+		//// check if ball x is less than 100
+		/*
+		for (GOval ball : balls) {
+            if (ball.getX() < 100) {
+                return; 
+            }
+        }
+        */
 		GOval ball = makeBall(SIZE/2, e.getY());
 		add(ball);
 	}
@@ -28,7 +57,9 @@ public class BallLauncher extends GraphicsProgram{
 		GOval temp = new GOval(x-SIZE/2, y-SIZE/2, SIZE, SIZE);
 		temp.setColor(Color.RED);
 		temp.setFilled(true);
+		balls.add(temp);
 		return temp;
+		
 	}
 	
 	public static void main(String[] args) {
